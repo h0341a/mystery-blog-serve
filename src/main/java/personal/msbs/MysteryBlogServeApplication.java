e22d5c1d5cead7370812b00affd26793f5a30e9c
h0341a <qianaiwuyu@gmail.com>
2020-01-09 18:27:42 +0800

    init

---
+++ src/main/java/personal/msbs/MysteryBlogServeApplication.java
@@ -0,0 +1,13 @@
+package personal.msbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysteryBlogServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysteryBlogServeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/personal/msbs/MysteryBlogServeApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.msbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysteryBlogServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
